6dbbae9caf9ef6b886b50036851e25b514c5bb35
Michael Gulotta <gulottam@gmail.com>
2023-06-09 16:28:36 -0400

    builds from state and completed flags

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
     return (
       <div>
         <h1>Todos</h1>
-        <ul>
-          <li>Walk the dog</li>
-          <li>Take out the trash</li>
-          <li>Workout</li>
+        <ul> 
+          {
+            todos.map(todo => {
+              return (<li>{todo.task} { todo.completed ? <span> - completed</span> : <span></span> }</li>)
+            })
+          }
         </ul>
         <form>
           <input/>
