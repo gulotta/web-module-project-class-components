1b7f8949e965241cddec06baf0eef773fa2e949f
Michael Gulotta <gulottam@gmail.com>
2023-06-09 17:52:08 -0400

    completed all tasks

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -22,16 +22,74 @@ export default class App extends React.Component {
       ]
     }
   }
+
+  handleAdd = (task) => {
+    // 1. setState
+    // 2. change todos
+    // 3. make a copy of todos
+    // 4. add a new todo to the end.
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+
+    });
+  }
+
+  handleClear = () => {
+    // 1. setState
+    // 2. loop through all todos
+    // 3. remove all todos that have completed === true
+    // 4. save filtered todos to state
+
+    this.setState({
+      ...this.state, 
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false );
+      })
+    
+  });
+  }
+
+  handleToggle = (clickedId) => {
+    // 1. setState
+    // 2. change todos
+    // 3. find todo that we clicked on
+    // 4. flip value of completed for that todo (false to true)
+    // 5. keep all other todos the same
+
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      })
+    })
+
+  }
+
   render() {
     const { todos } = this.state;
     return (
       <div>
         <h1>Todos</h1>
-        < TodoList todos={todos}/>
-        < Form />
-        <button>Clear</button>
+        < TodoList handleToggle={this.handleToggle} todos={todos}/>
+        < Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear</button>
 
       </div>
-    )
+    );
   }
 }
